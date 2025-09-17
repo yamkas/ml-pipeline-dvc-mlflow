ec129a012718502e8c51bb42b7973e82a33f0cc4
yamkas <yamkasudirman@gmail.com>
2025-09-17 15:28:42 +0700

    fresh

---
+++ src/evaluate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+
+
+os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/yamkas/ml-pipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME']="yamkas"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="9fb7d0ad24b5cde6bfa2a7a7002f09e00623f34a"
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/yamkas/ml-pipeline.mlflow")
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path,'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    mlflow.log_metric("accuracy",accuracy)
+    print("Model accuracy:{accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"],params["model"])
